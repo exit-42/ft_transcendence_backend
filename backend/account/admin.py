57cb974955abc9866f30235ee97cabfd6920fb24
uridonget <100407513+uridonget@users.noreply.github.com>
2025-01-17 16:59:04 +0900

    account 앱의 admin.py 파일에서 UserAdmin 클래스 search_fields 값을 수정. account 앱의 admin.py 파일에서 OAuthAdmin 클래스 search_fields 값을 수정. account 앱의 models.py 파일에서 imagePath 값이 blank인 경우를 허용하지 않도록 설정. account 앱의 models.py 파일에서 nickname 값이 null, blank인 경우를 허용하지 않도록 설정. models.py 파일과 admin.py 파일에 Doxygen 스타일의 주석을 작성.

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -7,12 +7,29 @@ from .models import User, OAuth
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    """
+    @brief User 모델을 Django Admin에 등록하기 위한 설정
+    @details 사용자 정보를 관리하며 목록, 검색, 필터링 기능을 제공
+
+    @list_display 목록에 표시할 필드
+    @search_fields 검색할 수 있는 필드
+    @list_filter 리스트 필터링 옵션
+    """
+
     list_display = ("username", "email", "nickname", "winCnt", "loseCnt", "is_staff")
-    search_fields = ("username", "email", "nickname")
+    search_fields = ("username", "nickname")
     list_filter = ("is_staff", "is_superuser")
 
 
 @admin.register(OAuth)
 class OAuthAdmin(admin.ModelAdmin):
+    """
+    @brief OAuth 모델을 Django Admin에 등록하기 위한 설정
+    @details OAuth 기능을 사용하는 사용자의 인증정보를 관리
+
+    @list_display 목록에 표시할 필드
+    @search_fields 검색할 수 있는 필드 (유저의 고유번호가 아니라 nickname으로 검색할 수 있도록 변경)
+    """
+
     list_display = ("intraId", "user")
-    search_fields = ("intraId", "user__username")
+    search_fields = ("intraId", "user__nickname")
