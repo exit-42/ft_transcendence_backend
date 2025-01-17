9382147bedb9b76a7790f30bb32112c575fe4f8a
uridonget <100407513+uridonget@users.noreply.github.com>
2025-01-17 12:48:08 +0900

    #7 feat(database): User, OAuth 클래스 admin에 추가.
    admin 페이지에서 DB를 관리할 수 있도록 admin.py 파일에 해당 클래스를 추가.

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User, OAuth
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "nickname", "winCnt", "loseCnt", "is_staff")
+    search_fields = ("username", "email", "nickname")
+    list_filter = ("is_staff", "is_superuser")
+
+
+@admin.register(OAuth)
+class OAuthAdmin(admin.ModelAdmin):
+    list_display = ("intraId", "user")
+    search_fields = ("intraId", "user__username")
--- backend/account/models.py
+++ backend/account/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class User(AbstractUser):
     imagePath = models.URLField(max_length=255, blank=True, null=True)
     nickname = models.CharField(max_length=50, unique=True, blank=True, null=True)
@@ -12,8 +13,11 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+
 class OAuth(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='oauth_account')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="oauth_account"
+    )
     intraId = models.CharField(max_length=255, unique=True, db_index=True)
 
     def __str__(self):
