f973706493f62401daccd00b3b15c6bdbb9771d6
uridonget <100407513+uridonget@users.noreply.github.com>
2025-01-17 12:32:09 +0900

    #7 feat(database): User, OAuth 테이블 추가
    account 앱의 models.py에 User 및 OAuth 모델 추가.
    settings.py에서 AUTH_USER_MODEL 설정을 커스텀 User 모델로 변경.
    entrypoint.sh에 account 앱의 마이그레이션 명령어 추가.

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    imagePath = models.URLField(max_length=255, blank=True, null=True)
+    nickname = models.CharField(max_length=50, unique=True, blank=True, null=True)
+    winCnt = models.PositiveIntegerField(default=0)
+    loseCnt = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.username
+
+class OAuth(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='oauth_account')
+    intraId = models.CharField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.intraId
