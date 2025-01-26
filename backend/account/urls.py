b79902aed3961dab9d5865cd0ee6cc080fecbd5b
uridonget <100407513+uridonget@users.noreply.github.com>
2025-01-26 15:03:49 +0900

    #15 feat(LocalAuth): 회원가입 이메일 인증 추가
    1. TLS 관련 설정 추가
        - 위치 : backend/transcendence/settings.py (154:155)
        - 장고가 nginx와 같은 프록시 서버 뒤에서 HTTPS 요청 여부를 인식하도록 돕는 설정.
        - 해당 장고 설정에 맞추어 프론트의 nginx 설정 파일 수정 필요.
    
    2. Send Mail 관련 설정 추가
        - 위치 : backend/transcendence/settings.py (157:172)
        - 장고가 smtp를 사용해서 이메일을 전송할 때 필요한 설정.
        - 해당 기능을 사용하려면 TLS 설정이 필요해서 위에서 추가하였음.
    
    3. CORS header 라이브러리 추가
        - 위치 : backend/requirements.txt (7:7)
        - 프론트에서 백엔드로 api 요청을 보낼 때 발생하는 CORS 요청을 허용하기 위한 라이브러리.
    
    4. CORS header 관련 설정 추가
        - 위치 : backend/transcendence/settings.py (44:44) (68:68) (174:189)
        - 개발단계라서 모든 출처의 모든 메소드를 허용하도록 설정해두었음.
        - 추후 회의를 통해 CORS 관련 설정을 어떻게 설정할지 합의가 필요.
        - 해당 장고 설정에 맞추어 프론트의 nginx 설정 파일 수정 필요.
    
    5. TLS 설정에 맞추어 함수 수정
        - 위치 : backend/account/views.py
        - (수정 전 : http://localhost) => (수정 전 : https://localhost)
        - JWT 발급 할 때 set_cookie의 설정을 False에서 True로 변경
    
    6. LocalAuth 회원가입 과정에서 입력한 이메일로 인증메일을 발송하는 send_authentication_email 함수 추가
        - 위치 : backend/account/views.py
        - 인증코드를 이메일로 전송하도록 요청하는 함수 추가.
    
    7. LocalAuth 회원가입 과정에서 인증코드를 제출하는 authenticate_code 함수 추가
        - 위치 : backend/account/views.py
        - 인증코드를 장고서버로 보내 발급한 인증코드와 비교하는 함수 추가.
    
    8. local_auth_sign_up 함수 수정
        - 위치 : backend/account/views.py
        - 이메일 인증여부를 확인하고 유저를 생성하도록 수정.

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("local-auth/token/", views.get_local_auth_token),
     path("local-auth/id/", views.check_local_auth_id),
     path("local-auth/signup/", views.local_auth_sign_up),
+    path("local-auth/email/", views.send_authentication_email),
+    path("local-auth/code/", views.authenticate_code),
 ]
