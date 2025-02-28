8d5461701af2fc4298751ba72b5fa6c6aac06342
allorak333 <allorak333@gmail.com>
2025-03-01 00:50:58 +0900

    chore(log) : 오류 상황 시 로그 기능 추가
    bool 반환으로 변경하여 외부에서 오류 상황을 감지할 수 있게 하였습니다.
    내부에서도 디버깅에 용이하게 오류 상황 시 로그를 남기게 하였습니다.

--- backend/log/utils.py
+++ backend/log/utils.py
@@ -6,10 +6,13 @@ User = get_user_model()
 
 from log.models import Game, Match
 
+import logging
+
+logger = logging.getLogger('django')
 
 def create_game(game_type):
     """
-    @brief game을 생성하는 함수 (테스트 용도로만 사용할 임시 함수)
+    @brief game을 생성하는 함수
 
     @param game_type
 
@@ -17,8 +20,7 @@ def create_game(game_type):
         - 게임 생성 성공 : gameId
         - 에러 발생 : None
 
-    @details
-    게임을 시작할 때 호출하는 내부 API
+    @details 게임을 시작할 때 호출하는 내부 API
     """
     try:
         is_tournament = False
@@ -30,43 +32,48 @@ def create_game(game_type):
         return game.gameId
 
     except Exception as e:
+        logger.debug("create_game :" + str(e))
         return None
 
 
 def set_game_end(game_id):
     """
-    @brief game의 진행상태를 종료로 변경하는 함수 (테스트 용도로만 사용할 임시 함수)
+    @brief game의 진행상태를 종료로 변경하는 함수
 
     @param game_id
 
     @return
-        - return 없음
-    @details
-    게임이 종료될 때 호출하는 내부 API
+        - 성공 : True
+        - 에러 발생 : False
+
+    @details 게임이 종료될 때 호출하는 내부 API
     """
     try:
         try:
             game = Game.objects.get(gameId=game_id)
         except Game.DoesNotExist:
-            return
+            logger.debug("set_game_end : game not exist " + str(game_id))
+            return False
 
         game.isEnd = True
         game.save()
 
-        return
+        return True
 
     except Exception as e:
-        return
+        logger.debug("set_game_end :" + str(e))
+        return False
 
 
 def create_match_log(result):
     """
-    @brief 게임 후 match 결과를 등록하는 함수 (테스트 용도로만 사용할 임시 함수)
+    @brief 게임 후 match 결과를 등록하는 함수
 
     @param player_A_id, player_B_id, score_A, score_B, game_id, rank가 담긴 dictionary
 
-    @return
-        - 없음
+    @return 
+        - 성공 : True
+        - 에러 발생 : False
 
     @details
     매치(1대1)가 종료될 때 호출하는 내부 API
@@ -82,7 +89,8 @@ def create_match_log(result):
             "rank",
         ]
         if not all(field in result for field in required_fields):
-            return
+            logger.debug("create_match_log : wrong argument")
+            return False
 
         player_A_id = result["player_A_id"]
         player_B_id = result["player_B_id"]
@@ -95,15 +103,18 @@ def create_match_log(result):
             player_A = User.objects.get(id=player_A_id)
             player_B = User.objects.get(id=player_B_id)
         except User.DoesNotExist:
-            return
+            logger.debug("create_match_log : user not exist")
+            return False
 
         try:
             game = Game.objects.get(gameId=game_id)
         except Game.DoesNotExist:
-            return
+            logger.debug("create_match_log : game not exist " + str(game_id))
+            return False
 
         if game.isEnd == True:
-            return
+            logger.debug("create_match_log : already game end")
+            return False
 
         Match.objects.create(
             playerA=player_A,
@@ -113,7 +124,8 @@ def create_match_log(result):
             game=game,
             rank=rank,
         )
-        return
+        return True
 
     except Exception as e:
-        return
+        logger.debug("create_match_log : " + str(e))
+        return False
